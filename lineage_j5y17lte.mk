43011a05ae752f0d5557b0a7c78c893c39fd0f06
Brian Gotte <bngotte@gmail.com>
2021-03-14 18:34:08 -0300

    j5y17lte: Define PRODUCT_MODEL

--- lineage_j5y17lte.mk
+++ lineage_j5y17lte.mk
@@ -29,6 +29,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 PRODUCT_DEVICE := j5y17lte
 PRODUCT_NAME := lineage_j5y17lte
 PRODUCT_BRAND := samsung
+PRODUCT_MODEL := Galaxy J5 2017
 PRODUCT_MANUFACTURER := samsung
 
 TARGET_VENDOR := samsung
