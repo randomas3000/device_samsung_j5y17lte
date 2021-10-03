8673acd4c6cc65d78d5afa0e6099d8538b4ddd67
Brian Gotte <bngotte@gmail.com>
2021-10-03 14:35:47 -0300

    Add J530G device codename

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -19,7 +19,7 @@ DEVICE_PATH := device/samsung/j5y17lte
 BOARD_USE_TFA_AMP := true
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := j5y17lte,j5y17ltexx,j5y17ltextc
+TARGET_OTA_ASSERT_DEVICE := j5y17lte,j5y17ltexx,j5y17ltextc,j5y17lteub
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
